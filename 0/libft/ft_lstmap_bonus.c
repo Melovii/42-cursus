4a89802414bc6134ef3e3847df8028f0683e234f
Beşir <mmunajed@k1m22s04.42istanbul.com.tr>
2024-10-12 13:02:12 +0300

    fixed implementation

--- 0/libft/ft_lstmap_bonus.c
+++ 0/libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mmunajed <mmunajed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:51:02 by mmunajed          #+#    #+#             */
-/*   Updated: 2024/10/07 15:18:00 by mmunajed         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:01:11 by mmunajed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_llist;
-	t_list	*new_element;
+	t_list	*new_list;
+	t_list	*new_obj;
 
-	if (!lst)
+	if (!lst || !f || !del)
 		return (NULL);
-	new_llist = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		new_element = ft_lstnew(f(lst->content));
-		if (!new_element)
+		new_obj = ft_lstnew(f(lst->content));
+		if (!new_obj)
 		{
-			ft_lstclear(&new_element, del);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		else
-			ft_lstadd_back(&new_llist, new_element);
-		lst = lst -> next;
+		ft_lstadd_back(&new_list, new_obj);
+		lst = lst->next;
 	}
-	return (new_llist);
+	return (new_list);
 }
--- 0/libft/ft_split.c
+++ 0/libft/ft_split.c
@@ -6,85 +6,102 @@
 /*   By: mmunajed <mmunajed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:42:06 by mmunajed          #+#    #+#             */
-/*   Updated: 2024/10/07 14:08:42 by mmunajed         ###   ########.fr       */
+/*   Updated: 2024/10/12 12:32:32 by mmunajed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
 
-static int	char_is_separator(char c, char charset)
+static int	word_count(const char *str, char c);
+static char	*fill_word(const char *str, int start, int end);
+static void	*ft_free(char **strs, int count);
+static void	ft_initiate_vars(size_t *i, int *j, int *s_word);
+
+char	**ft_split(const char *s, char c)
 {
-	if (c == charset || c == '\0')
-		return (1);
-	return (0);
-}
+	char	**res;
+	int		s_word;
+	int		j;
+	size_t	i;
 
-static int	count_words(char *str, char c)
-{
-	int	i;
-	int	words;
-
-	words = 0;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (char_is_separator(str[i], c) == 0
-			&& (i == 0 || char_is_separator(str[i - 1], c) == 1))
-			words++;
-		i++;
-	}
-	return (words);
-}
-
-void	write_word(char *dest, char *from, char c)
-{
-	int	i;
-
-	i = 0;
-	while (char_is_separator(from[i], c) == 0)
-	{
-		dest[i] = from[i];
-		i++;
-	}
-	dest[i] = '\0';
-}
-
-static void	write_split(char **split, char *str, char c)
-{
-	int	word;
-	int	i;
-	int	j;
-
-	word = 0;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (char_is_separator(str[i], c) == 1)
-			i++;
-		else
-		{
-			j = 0;
-			while (char_is_separator(str[i + j], c) == 0)
-				j++;
-			split[word] = (char *)malloc(sizeof(char) * (j + 1));
-			write_word(split[word], str + i, c);
-			i += j;
-			word++;
-		}
-	}
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**strings;
-	int		words;
-
-	words = count_words((char *)s, c);
-	strings = (char **)malloc(sizeof(char *) * (words + 1));
-	if (strings == NULL)
+	ft_initiate_vars(&i, &j, &s_word);
+	res = ft_calloc((word_count(s, c) + 1), sizeof(char *));
+	if (!res)
 		return (NULL);
-	strings[words] = 0;
-	write_split(strings, (char *)s, c);
-	return (strings);
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && s_word < 0)
+			s_word = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		{
+			res[j] = fill_word(s, s_word, i);
+			if (!(res[j]))
+				return (ft_free(res, j));
+			s_word = -1;
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+static void	ft_initiate_vars(size_t *i, int *j, int *s_word)
+{
+	*i = 0;
+	*j = 0;
+	*s_word = -1;
+}
+
+static void	*ft_free(char **strs, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (NULL);
+}
+
+static char	*fill_word(const char *str, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = 0;
+	return (word);
+}
+
+static int	word_count(const char *str, char c)
+{
+	int	count;
+	int	x;
+
+	count = 0;
+	x = 0;
+	while (*str)
+	{
+		if (*str != c && x == 0)
+		{
+			x = 1;
+			count++;
+		}
+		else if (*str == c)
+			x = 0;
+		str++;
+	}
+	return (count);
 }
