1dcb2a7f4b102ab1cb09ca6a0d755bfcc5beb574
Beşir <mmunajed@k1m22s04.42istanbul.com.tr>
2024-10-12 10:42:25 +0300

    fixed function name

--- 0/libft/ft_calloc.c
+++ 0/libft/ft_calloc.c
@@ -6,13 +6,13 @@
 /*   By: mmunajed <mmunajed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:07:29 by mmunajed          #+#    #+#             */
-/*   Updated: 2024/10/07 14:05:55 by mmunajed         ###   ########.fr       */
+/*   Updated: 2024/10/12 10:37:58 by mmunajed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 	size_t	total_size;
--- 0/libft/libft.h
+++ 0/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmunajed <mmunajed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:53:21 by mmunajed          #+#    #+#             */
-/*   Updated: 2024/10/10 10:16:03 by mmunajed         ###   ########.fr       */
+/*   Updated: 2024/10/12 10:38:05 by mmunajed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void		*ft_memset(void *s, int c, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
-void		*calloc(size_t count, size_t size);
+void		*ft_calloc(size_t count, size_t size);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 /* -=-=-=-=-	FILES		=-=-=-=-=-=- */
