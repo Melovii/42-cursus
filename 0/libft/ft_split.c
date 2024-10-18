ac87d0b496b503ba0023f66e148c8d0998c645fa
Melovi MAC <145908254+Melovii@users.noreply.github.com>
2024-10-18 10:41:32 +0300

    changed up naming conventions

--- 0/libft/ft_split.c
+++ 0/libft/ft_split.c
@@ -3,105 +3,120 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmunajed <mmunajed@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Melovi <Melovi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:42:06 by mmunajed          #+#    #+#             */
-/*   Updated: 2024/10/16 16:28:01 by mmunajed         ###   ########.fr       */
+/*   Updated: 2024/10/18 10:35:26 by Melovi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(const char *str, char c);
-static char	*fill_word(const char *str, int start, int end);
-static void	*ft_free(char **strs, int count);
-static void	ft_initiate_vars(size_t *i, int *j, int *s_word);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Melovi <Melovi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 12:42:06 by mmunajed          #+#    #+#             */
+/*   Updated: 2024/10/18 10:22:27 by Melovi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char	**ft_split(const char *s, char c)
+#include "libft.h"
+
+static int	count_words(const char *s, char c);
+static char	*extract_word(const char *s, int start_i, int end_i);
+static void	*free_memory(char **words_arr, int allocated_count);
+static void	init_vars(size_t *curr_i, int *word_i, int *start_word_i);
+
+char	**ft_split(char const *s, char c)
 {
-	char	**res;
-	int		s_word;
-	int		j;
-	size_t	i;
+	char	**words_arr;
+	int		start_word_i;
+	int		word_i;
+	size_t	curr_i;
 
-	ft_initiate_vars(&i, &j, &s_word);
-	res = ft_calloc((word_count(s, c) + 1), sizeof(char *));
-	if (!res)
+	init_vars(&curr_i, &word_i, &start_word_i);
+	words_arr = ft_calloc((count_words(s, c) + 1), sizeof(char *));
+	if (!words_arr)
 		return (NULL);
-	while (i <= ft_strlen(s))
+	while (curr_i <= ft_strlen(s))
 	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		if (s[curr_i] != c && start_word_i < 0)
+			start_word_i = curr_i;
+		else if ((s[curr_i] == c
+				|| curr_i == ft_strlen(s)) && start_word_i >= 0)
 		{
-			res[j] = fill_word(s, s_word, i);
-			if (!(res[j]))
-				return (ft_free(res, j));
-			s_word = -1;
-			j++;
+			words_arr[word_i] = extract_word(s, start_word_i, curr_i);
+			if (!(words_arr[word_i]))
+				return (free_memory(words_arr, word_i));
+			start_word_i = -1;
+			word_i++;
 		}
-		i++;
+		curr_i++;
 	}
-	return (res);
+	return (words_arr);
 }
 
-static void	ft_initiate_vars(size_t *i, int *j, int *s_word)
+static void	init_vars(size_t *curr_i, int *word_i, int *start_word_i)
 {
-	*i = 0;
-	*j = 0;
-	*s_word = -1;
+	*curr_i = 0;
+	*word_i = 0;
+	*start_word_i = -1;
 }
 
-static void	*ft_free(char **strs, int count)
+static void	*free_memory(char **words_arr, int allocated_count)
 {
 	int	i;
 
 	i = 0;
-	while (i < count)
+	while (i < allocated_count)
 	{
-		free(strs[i]);
+		free(words_arr[i]);
 		i++;
 	}
-	free(strs);
+	free(words_arr);
 	return (NULL);
 }
 
-static char	*fill_word(const char *str, int start, int end)
+static char	*extract_word(const char *s, int start_i, int end_i)
 {
 	char	*word;
 	int		i;
 
 	i = 0;
-	word = malloc((end - start + 1) * sizeof(char));
+	word = malloc((end_i - start_i + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
-	while (start < end)
+	while (start_i < end_i)
 	{
-		word[i] = str[start];
+		word[i] = s[start_i];
 		i++;
-		start++;
+		start_i++;
 	}
 	word[i] = 0;
 	return (word);
 }
 
-static int	word_count(const char *str, char c)
+static int	count_words(const char *s, char c)
 {
-	int	count;
-	int	x;
+	int	word_count;
+	int	is_word;
 
-	count = 0;
-	x = 0;
-	while (*str)
+	word_count = 0;
+	is_word = 0;
+	while (*s)
 	{
-		if (*str != c && x == 0)
+		if (*s != c && is_word == 0)
 		{
-			x = 1;
-			count++;
+			is_word = 1;
+			word_count++;
 		}
-		else if (*str == c)
-			x = 0;
-		str++;
+		else if (*s == c)
+			is_word = 0;
+		s++;
 	}
-	return (count);
+	return (word_count);
 }
