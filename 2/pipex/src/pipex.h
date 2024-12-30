857f54b38002d0170574426aa1ffce8e00ca906a
melovi <145908254+Melovii@users.noreply.github.com>
2024-12-30 21:39:48 +0300

    added function prototypes and headers for pipex

--- 2/pipex/src/pipex.h
+++ 2/pipex/src/pipex.h
@@ -1,8 +1,12 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
+// TODO: get rid of unnecessary headers and consider changing structure
+// ! don't forget about adding only necessary headers for BOTH files (ask co-workers for affirmation)
+
+# include "../libft/libft.h"
 # include <unistd.h> // pipe, write, read maybe
-# include <stdlib.h> // malloc
+# include <stdlib.h> // malloc // didn't use malloc lol
 # include <fcntl.h> // open
 # include <errno.h> // for perror()
 # include <stdio.h> // for perror() too lol
@@ -11,5 +15,11 @@
 // For example:
 // - find_command_path() to search for the full path of a command
 // - Any utility functions for string manipulation (ft_split, ft_strjoin, etc.)
+
+int		ft_open_file(char* file_name, int flow);
+char	*ft_find_cmd(char* cmd, char** envp);
+char	*ft_find_envp(char **envp);
+void	ft_free_tab(char** tab);
+void	error_handler(int n_error);
 
 #endif
