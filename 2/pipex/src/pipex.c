125c9c579834b7474ffca94c47363178855e182f
melovi <145908254+Melovii@users.noreply.github.com>
2024-12-31 14:30:40 +0300

    updated error handling and error messages

--- 2/pipex/src/pipex.c
+++ 2/pipex/src/pipex.c
@@ -5,13 +5,7 @@
 // * example usage: < infile grep a1 | wc -w > outfile
 // * example usage: ./pipex infile "grep a1" "wc -w" outfile
 
-// TODO: DECIDE HOW TO HANDLE SUCH ERRORS THAT WOULD TAKE A LOT OF LINES TO HANDLE BUT DON'T FALL INTO A CATEGORY IN ERROR_HANDLER()
-// TODO: if input file doesn't exist, set bytes to 0 on the output file
-// TODO: add messages based on the subject.pdf for when it fails
-// TODO: check all the frees that I've used.. are they even necessary?
-// TODO: test the hell out of the program lol (oh and uhh.. wtf about leaks? lol)
-
-// * oh btw consider making the error_handler work with 2 parameters (with msg)
+// TODO: if input file doesn't exist, set bytes to 0 on the output file // uhh what lol is this for cat?
 
 static void	ft_execute(char* cmd, char** envp)
 {
@@ -23,37 +17,33 @@ static void	ft_execute(char* cmd, char** envp)
 	
 	if(execve(path, cmds, envp) == -1)
 	{
-		// ? can not understand this check...
-		ft_putstr_fd("pipex: command not found: ", 2);
-		ft_putendl_fd(cmds[0], 2);
+		ft_putstr_fd(cmds[0], 2);
+		ft_putendl_fd(": command not found ", 2);
 		ft_free_tab(cmds);
 		exit(0);
 	}
 }
 
-static void	child(char** argv, int* chan, char** envp)
+static void	child(char** argv, int* chan, char** envp) // * reading end
 {
 	int	fd;
 
-	// ! pipe[0] = input (read) = 0
 	fd = ft_open_file(argv[1], 0);
-
 	dup2(fd, 0);
 	dup2(chan[1], 1);
 	close(chan[0]);
-	ft_execute(argv[2], envp); // execute cmd1
+	ft_execute(argv[2], envp); // * execute cmd1
 }
 
-static void	parent(char** argv, int* chan, char** envp)
+static void	parent(char** argv, int* chan, char** envp) // * writing end
 {
 	int	fd;
 
-	// ! pipe[1] = output (write) = 1
 	fd = ft_open_file(argv[4], 1);
 	dup2(fd, 1);
 	dup2(chan[0], 0);
 	close(chan[1]);
-	ft_execute(argv[3], envp); // execute cmd2
+	ft_execute(argv[3], envp); // * execute cmd2
 }
 
 int main(int argc, char** argv, char** envp)
@@ -62,19 +52,24 @@ int main(int argc, char** argv, char** envp)
 	pid_t	pid;
 
 	if (argc != 5)
-		error_handler(1);
-
+	{
+		ft_putstr_fd("Proper Usage: ./pipex infile \"cmd1\" \"cmd2\" outfile\n", 2);
+		exit(0);
+		// error_handler(1);
+	}
 	if (pipe(chan) < 0)
+	{
 		exit(-1);
-
+	}
 	pid = fork();
 	if (pid < 0)
+	{
 		exit(-1);
-
+	}
 	if (pid == 0)
+	{
 		child(argv, chan, envp);
-
+	}
 	parent(argv, chan, envp);
-
 	return (0);
 }
--- 2/pipex/src/pipex_utils.c
+++ 2/pipex/src/pipex_utils.c
@@ -1,15 +1,14 @@
 #include "pipex.h"
 
-// Handle different error cases for the program.
-// If n_error == 1, print an error message about incorrect command usage and exit.
+// TODO: remove error_handler function (not being used lol);
+
 void	error_handler(int n_error)
 {
 	if (n_error == 1)
-		ft_putstr_fd("./pipex infile cmd | cmd outfile\n", 2);
+		ft_putstr_fd("Proper Usage: ./pipex infile \"cmd1\" \"cmd2\" outfile\n", 2);
 	exit(0); 
 }
 
-// Open a file with the given file name and mode (flow). Handle errors appropriately.
 int	ft_open_file(char* file_name, int flow)
 {
 	int	fd;
@@ -18,15 +17,14 @@ int	ft_open_file(char* file_name, int flow)
 		fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	else
 		fd = open(file_name, O_RDONLY, 0777);
-
 	if (fd < 0)
+	{
+		ft_putstr_fd("bash: ", 2);
+		perror(file_name);
 		exit(0);
-
+	}
 	return (fd);
 }
-
-// TODO: Implement ft_free_tab function
-// This function frees memory allocated for a table (array) of strings.
 
 void	ft_free_tab(char** tab)
 {
@@ -41,8 +39,6 @@ void	ft_free_tab(char** tab)
 	free(tab);
 }
 
-// TODO: Implement ft_get_envp function
-// This function retrieves a specific environment variable's value from the environment variables array.
 char	*ft_find_envp(char **envp)
 {
 	int		i;
@@ -58,14 +54,11 @@ char	*ft_find_envp(char **envp)
 		}
 		i++;
 	}
-
 	if (!envp_path)
 		exit(1);
-
 	return (envp_path);
 }
-// TODO: Implement ft_find_cmd function
-// This function will search for the full path of the given command in the directories listed in the PATH envpironment variable.
+
 char	*ft_find_cmd(char* cmd, char** envp)
 {
 	char**	cmd_options;
@@ -74,24 +67,22 @@ char	*ft_find_cmd(char* cmd, char** envp)
 	char*	exec;
 	int		index;
 
-	index = -1; // don't use 0 lol
+	index = -1; // * don't use 0 lol
 	path = ft_split(ft_find_envp(envp), ':');
 	cmd_options = ft_split(cmd, ' ');
 	while (path[++index])
 	{
-		path_part = ft_strjoin(path[index], "/"); // basically from /usr/bin to /usr/bin
+		path_part = ft_strjoin(path[index], "/"); // * basically from /usr/bin to /usr/bin
 		exec = ft_strjoin(path_part, cmd_options[0]);
-		// ? ? ? ? ? ? ? ? ?
-		free(path_part); // ! WHY FREE?
+		free(path_part);
 		if (access(exec, F_OK | X_OK) == 0)
 		{
 			ft_free_tab(cmd_options);
-			return (exec); // ? why return exec if we're dead?
+			return (exec); // ! why return exec if we're dead?
 		}
 		free(exec);
 	}
 	ft_free_tab(path);
-	// ? ? ? ? ? ? ? ? ? ? ? ? ? ?
-	ft_free_tab(cmd_options); // ! WHY FREE?
+	ft_free_tab(cmd_options);
 	return (cmd);
 }
