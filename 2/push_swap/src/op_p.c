8220cfc516394b01ef5a282b484b7de22b30390b
melovi <145908254+Melovii@users.noreply.github.com>
2025-01-04 11:03:35 +0300

    implemented push stack operation

---
+++ 2/push_swap/src/op_p.c
@@ -0,0 +1,30 @@
+#include "push_swap.h"
+
+// * Pushes from B to A
+void	pa(t_liist **stack_a, t_liist **stack_b)
+{
+	t_liist	*push_b;
+
+	if (!*stack_a)
+		return ;
+
+	push_b = (*stack_b)->next;
+	(*stack_b)->next = *stack_a;
+	*stack_a = *stack_b;
+	*stack_b = push_b;
+	ft_putstr_fd("pa\n", 1);
+}
+
+// * Pushes from A to B
+void	pb(t_liist **stack_a, t_liist **stack_b)
+{
+	t_liist	*push_a;
+
+	if (!*stack_b)
+		return ;
+	push_a = (*stack_a)->next;
+	(*stack_a)->next = (*stack_b);
+	*stack_b = *stack_a;
+	*stack_a = push_a;
+	ft_putstr_fd("pb\n", 1);
+}
