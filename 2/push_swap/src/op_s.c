eea744fe35248754bc08eec5448f89761d19700f
melovi <145908254+Melovii@users.noreply.github.com>
2025-01-04 11:03:35 +0300

    added short description for functions

--- 2/push_swap/src/op_s.c
+++ 2/push_swap/src/op_s.c
@@ -12,17 +12,21 @@ static void	ft_s(t_liist **stack)
 	(*stack)->next = tmp;
 }
 
+// * Swaps top two values in stack A
 void	sa(t_liist** stack_a)
 {
 	ft_s(stack_a);
 	write(1, "sa\n", 3);
 }
+
+// * Swaps top two values in stack A
 void	sb(t_liist** stack_b)
 {
 	ft_s(stack_b);
 	write(1, "sb\n", 3);
 }
 
+// * Does both sa and sb
 void	ss(t_liist** stack_a, t_liist** stack_b)
 {
 	ft_s(stack_a);
