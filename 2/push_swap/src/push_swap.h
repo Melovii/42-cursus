f778cb3887d268c678c7e624a0b88f4c5c250ce2
melovi <145908254+Melovii@users.noreply.github.com>
2025-01-05 22:05:21 +0300

    added function prototypes

--- 2/push_swap/src/push_swap.h
+++ 2/push_swap/src/push_swap.h
@@ -4,6 +4,16 @@
 # include "../libft/libft.h"
 # include <limits.h>
 
+// TODO: consider updating implementation to work with libft's t_list struct
+
+typedef struct	s_liist
+{
+	long int		content;
+	int				flag;
+	int				index;
+	struct s_liist	*next;
+}				t_liist;
+
 typedef struct	s_push
 {
 	int	next;
@@ -21,5 +31,21 @@ typedef struct	s_swap
 }				t_swap;
 
 // ! include function prototypes here
+
+void	pa(t_liist **stack_a, t_liist **stack_b);
+void	pb(t_liist **stack_a, t_liist **stack_b);
+
+void	sa(t_liist** stack_a);
+void	sb(t_liist** stack_b);
+void	ss(t_liist** stack_a, t_liist** stack_b);
+
+void	ra(t_liist **stack_a);
+void	rb(t_liist **stack_b);
+void	rr(t_liist **stack_a, t_liist **stack_b);
+
+void	rra(t_liist *stack_a);
+void	rrb(t_liist *stack_b);
+void	rrr(t_liist *stack_a, t_liist *stack_b);
+
 
 #endif
