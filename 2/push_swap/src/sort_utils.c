1e1dc9803e1e43eb0a9516542917b8b3f73e6768
melovi <145908254+Melovii@users.noreply.github.com>
2025-01-21 18:31:43 +0300

    Finished utility functions for push_swap

---
+++ 2/push_swap/src/sort_utils.c
@@ -0,0 +1,76 @@
+#include "push_swap.h"
+
+// * Checks if the list is sorted in ascending order
+int	is_sorted(t_liist **stack)
+{
+	t_list	*tmp;
+
+	tmp = *stack;
+	while (tmp && tmp->next)
+	{
+		if (tmp->content > tmp->next->content)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+// * Checks if list is sorted in descending order.
+int	is_rev_sorted(t_swap *tab)
+{
+	t_list	*tmp;
+
+	tmp = tab->stack_a;
+	while (tmp)
+	{
+		if (tmp->content < tmp->next->content)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+// * Checks if the list is sorted and if its size matches the given count.
+int	lst_valid(t_liist **stack, int count)
+{
+	int	len;
+
+	len = ft_lstsize(*stack);
+	if (len != count)
+		return (0);
+	if (is_sorted(stack) == 0)
+		return (0);
+	return (1);
+}
+
+// * Finds minimum element in the list.
+t_list	*find_min_lst(t_list **stack)
+{
+	t_list	*tmp;
+	t_list	*min;
+
+	tmp = *stack;
+	min = *stack;
+	while (tmp)
+	{
+		if (min->content > tmp->content)
+			min = tmp;
+		tmp = tmp->next;
+	}
+}
+
+// * Finds maximum element in the list.
+t_list	*find_max_lst(t_list **stack)
+{
+	t_list	*tmp;
+	t_list	*max;
+
+	tmp = *stack;
+	max = *stack;
+	while (tmp)
+	{
+		if (max->content < tmp->content)
+			max = tmp;
+		tmp = tmp->next;
+	}
+}
