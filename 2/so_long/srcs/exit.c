e6e84cabaf39414b3fca96cf62048fc4adb116c3
melovi <145908254+Melovii@users.noreply.github.com>
2025-02-16 17:58:22 +0300

    finished functions for freeing the program and destroying window with no issues or leaks

---
+++ 2/so_long/srcs/exit.c
@@ -0,0 +1,58 @@
+#include "../so_long.h"
+
+void	ft_free(void *vars)
+{
+	if (vars)
+		free(vars);
+	vars = NULL;
+}
+
+void	destroy_img(t_vars *vars)
+{
+	int	i;
+
+	if (vars->mlx)
+	{
+		if (vars->img)
+		{
+			mlx_destroy_image(vars->mlx, vars->img->img); // ? why img->img?
+			ft_free(vars->img);
+		}
+		i = 0;
+		while (i < 7)
+			mlx_destroy_image(vars->mlx, vars->tex[i++].img);
+		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_display(vars->mlx);
+		ft_free(vars->mlx);
+	}
+}
+
+void	free_vars(t_vars *vars)
+{
+	int	i;
+
+	if (vars)
+	{
+		destroy_img(vars);
+		if (vars->key)
+			ft_free(vars->key);
+		if (vars->pos)
+			ft_free(vars->pos);
+		i = 0;
+		if (vars->map)
+		{
+			while (vars->map[i])
+				ft_free(vars->map[i++]);
+			ft_free(vars->map);
+		}
+		ft_free(vars);
+	}
+}
+
+int	ft_exit(char *msg, t_vars *vars, int ret)
+{
+	if (msg)
+		ft_putstr_fd(msg, 2);
+	free_vars(vars);
+	exit(ret);
+}
