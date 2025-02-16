5a72ccbc00e7c3efcf46ec162a60d774f69388f1
melovi <145908254+Melovii@users.noreply.github.com>
2025-02-16 18:03:00 +0300

    initialised main function

--- 2/so_long/srcs/main.c
+++ 2/so_long/srcs/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include "../so_long.h"
 
-int	main(void)
+// * Initializes the game, checks arguments, allocates memory, and sets up MLX
+int	main(int argc, char **argv)
 {
-	printf("test");
+	t_vars	*vars;
+
+	if (argc != 2)
+		ft_exit("Error\n Bad argument!\n", NULL, FAILURE);
+	vars = ft_calloc(sizeof(t_vars), 1); // ! Explain why I used ft_calloc and not malloc here
+	if (!vars)
+		ft_exit("Error\n Can't malloc (vars)!\n", vars, FAILURE);
+	vars->pos = malloc(sizeof(t_pos));
+	if (!vars->pos)
+		ft_exit("Error\n Can't malloc (pos)!\n", vars, FAILURE);
+	vars->key = init_key();	// TODO: init_key function
+	if (!vars->key)
+		ft_exit("Error\n Can't malloc (key)!\n", vars, FAILURE);
+	parsing(argv[1], vars);	// TODO: parsing function
+	vars->img = malloc(sizeof(t_img));
+	if (!vars->img)
+		ft_exit("Error\n Can't malloc (img)!\n", vars, FAILURE);
+	vars->mlx = mlx_init();	// TODO: init_mlx function
+	if (!vars->mlx)
+		ft_exit("Error\n Can't init mlx!\n", vars, FAILURE);
+	init_program(vars);	// TODO: init_program function
 	return (0);
 }
