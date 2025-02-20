aacad5662c1aca1c8f05365252c2c50035f77e04
melovi <145908254+Melovii@users.noreply.github.com>
2025-02-20 15:40:08 +0300

    refactored code for clarity

--- 2/so_long/srcs/validation.c
+++ 2/so_long/srcs/validation.c
@@ -2,32 +2,23 @@
 
 int	check_is_file(char *argv, t_vars *vars)
 {
-	int	i;
+	char	*ext;
 
-	i = ft_strlen(argv);
-	while (argv[i] != '.')
-	{
-		if (i == 0)
-			ft_exit("Error\nInvalid file\n", vars, FAILURE);
-		i--;
-	}
-	i++;
-	if (ft_strncmp((argv + i), "ber", ft_strlen(argv)) != 0) // ? CHECK IF STRLEN(ARGV[I]) PART WORKS OR NOT
+	ext = ft_strrchr(argv, '.');
+	if (!ext || ft_strncmp(ext, ".ber", strlen(argv)) != 0)
 		ft_exit("Error\nInvalid file extension\n", vars, FAILURE);
 	return (1);
 }
 
 void	validate_elements(int *nbr, int r, t_vars *vars)
 {
-	int	i;
-
-	i = 0;
 	if (!r)
 	{
-		while (i < 3)
-		{
-			if (nbr[i++] < 1 || nbr[2] > 1)
-				ft_exitc(4, vars, FAILURE);
-		}
+		if (nbr[0] < 1) // At least one collectible
+			ft_exitc(4, vars, FAILURE);
+		if (nbr[1] != 1) // Exactly one player
+			ft_exitc(4, vars, FAILURE);
+		if (nbr[2] != 1) // Exactly one exit
+			ft_exitc(4, vars, FAILURE);
 	}
 }
